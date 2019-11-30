38d239329f1309748d1e7b8f828cc6895307bad5
painter567 <1933908423@qq.com>
2019-11-30 21:00:25 +0800

    Initial commit

---
+++ src/main/java/com/ww/dao/ee.java
@@ -0,0 +1,8 @@
+package com.ww.dao;
+
+public class ee {
+    public void sout(){
+        System.out.println("22");
+
+    }
+}
---
+++ src/test/java/ww.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import com.ww.dao.ee;
+
+
+public class ww {
+    @Test
+    public void main(){
+        ee ee=new ee();
+        ee.sout();
+    }
+
+
+}
